30f9859bf8917147407340b75441881fa63003c6
Smerlin Hollender <106546281+1Smerlin@users.noreply.github.com>
2023-02-21 17:29:08 +0100

    Startserver up

--- StartServer.bat
+++ StartServer.bat
@@ -1,3 +1,8 @@
 @echo off
-start http://127.0.0.1:8000/hauptverzeichnis/home/home.html
+for /f "tokens=2 delims=: " %%f in ('nslookup %computername% ^| findstr /c:"Address:" ^| findstr /v /c:"#"') do set ipv4=%%f
+if "%ipv4%" == "" (
+    echo Visit your new website http://127.0.0.1:8000/hauptverzeichnis/home/home.html
+) else (
+echo Visit your new website http://%ipv4%:8000/hauptverzeichnis/home/home.html
+)
 node server.js
--- StartServer.sh
+++ StartServer.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ipv4=$(ifconfig | grep "inet " | awk '{print $2}' | grep -E "^192\.168\.[0-9]+\.[0-9]+")
 if [ -z "$ipv4" ]; then
-    echo "Visit your new website 127.0.0.1:8000/hauptverzeichnis/home/home.html"
+    echo "Visit your new website http://127.0.0.1:8000/hauptverzeichnis/home/home.html"
+else
+    echo "Visit your new website http://$ipv4:8000/hauptverzeichnis/home/home.html"
 fi
-echo "Visit your new website $ipv4:8000/hauptverzeichnis/home/home.html"
 node server.js
